83666cfd83d74f5945a9154f60781d4b89fb79a3
ChunGang5 <1330431376@qq.com>
2019-10-24 23:05:42 +0800

    将十六进制地址转换为点分十进制

---
+++ hex2dd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(int argc, char** argv[])
+{
+	struct in_addr inaddr;
+	uint32_t addr;
+	char buf[MAXBUF];
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "usage:%s <hex number>\n", argv[10]);
+		exit(0);
+	}
+	sscanf(argv[1], "%x", &addr);
+	inaddr.s_addr = htonal(addr);
+
+	if(!inet_ntop(AF_INET, &inaddr, buf, MAXBUF))
+		unix_error("inet_ntop");
+	printf("%s\n", buf);
+	
+	exit(0);
+	
+}
