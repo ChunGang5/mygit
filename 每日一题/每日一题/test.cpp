b155ebb6f78d62086f7a657fce7b8b9279933e87
ChunGang5 <1330431376@qq.com>
2019-11-29 00:20:32 +0800

    兔子-斐波那契

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -208,26 +208,59 @@ int main()
 
 	   */
 
+
+/*
+题目描述
+有一只兔子，从出生后第3个月起每个月都生一只兔子，小兔子长到第三个月后每个月又生一只兔子，假如兔子都不死，问每个月的兔子总数为多少？
+
+
+
+/**
+* 统计出兔子总数。
+*
+* @param monthCount 第几个月
+* @return 兔子总数
+
+public static int getTotalCount(int monthCount)
+{
+	return 0;
+}
+
+
+
+
+
+输入描述:
+输入int型表示month
+
+输出描述 :
+输出兔子总数int型
+
+示例1
+输入
+复制
+9
+输出
+复制
+34
 #include <stdio.h>
-#include <stdlib.h>
 int main()
 {
 	int month = 0;
-	while ((scanf("%d", month)) != EOF)
+	int i = 0;
+	while (scanf("%d", &month) != EOF)
 	{
-		int i;
-		long sum;
-		long x = 0, y = 0;
-		if (month>2)
+		int num1 = 1;
+		int num2 = 1;
+		int temp;
+		for (i = 3; i <= month; ++i)
 		{
-			for (i = 3; i <= month; i++)
-			{
-				sum = x + y;
-				x = y;
-				y = sum;
-			}
+			temp = num1 + num2;
+			num1 = num2;
+			num2 = temp;
 		}
-		printf("%d\n", sum);
+		printf("%d\n", temp);
 	}
 	return 0;
 }
+*/
