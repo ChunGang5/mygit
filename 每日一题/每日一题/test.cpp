3e71944e40342279b6e22a31016cbcc3099d877f
ChunGang5 <1330431376@qq.com>
2019-11-30 23:51:28 +0800

    用ASCII码解有关字符串问题

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -1,4 +1,51 @@
+/*链接：https://www.nowcoder.com/questionTerminal/784efd40ed8e465a84821c8f3970b7b5?f=discussion
+来源：牛客网
 
+[编程题]字符集合
+热度指数：37389时间限制：C / C++ 1秒，其他语言2秒空间限制：C / C++ 32M，其他语言64M
+算法知识视频讲解
+输入一个字符串，求出该字符串包含的字符集合
+
+输入描述 :
+每组数据输入一个字符串，字符串最大长度为100，且只包含字母，不可能为空串，区分大小写。
+
+
+输出描述 :
+每组数据一行，按字符串原有的字符顺序，输出字符集合，即重复出现并靠后的字母不输出。
+示例1
+输入
+abcqweracb
+输出
+abcqwer
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str;
+	while (cin >> str)
+	{
+		char arr[123] = { 0 };
+		int length = str.length();
+		int i = 0;
+		for (i = 0; i<length; i++)
+		{
+			arr[str[i]] = 1;
+		}
+		for (i = 0; i<length; i++)
+		{
+			if (arr[str[i]] == 1)
+			{
+				cout << str[i];
+				arr[str[i]] = 0;
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+*/
 /*链接：https://www.nowcoder.com/questionTerminal/2d3f6ddd82da445d804c95db22dcc471?orderByHotValue=1&page=1&onlyReference=false
 来源：牛客网
 
