f350baa104f66fa2fc98dc048410ebb949f8a8a3
ChunGang5 <1330431376@qq.com>
2019-12-02 23:06:10 +0800

    最大公约数——网易

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -1,3 +1,62 @@
+/*题目描述
+小易经常沉迷于网络游戏.有一次, 他在玩一个打怪升级的游戏, 他的角色的初始能力值为 a.在接下来的一段时间内, 他将会依次遇见n个怪物, 每个怪物的防御力为b1, b2, b3...bn.如果遇到的怪物防御力bi小于等于小易的当前能力值c, 那么他就能轻松打败怪物, 并 且使得自己的能力值增加bi; 如果bi大于c, 那他也能打败怪物, 但他的能力值只能增加bi 与c的最大公约数.那么问题来了, 在一系列的锻炼后, 小易的最终能力值为多少 ?
+输入描述 :
+	 对于每组数据, 第一行是两个整数n(1≤n<100000)表示怪物的数量和a表示小易的初始能力值.
+	 第二行n个整数, b1, b2...bn(1≤bi≤n)表示每个怪物的防御力
+ 输出描述 :
+对于每组数据, 输出一行.每行仅包含一个整数, 表示小易的最终能力值
+示例1
+输入
+复制
+3 50
+50 105 200
+5 20
+30 20 15 40 100
+输出
+复制
+110
+205
+#include<iostream>
+#include<vector>
+using namespace std;
+int gong_yue_num(int b, int c)
+{
+	int d = 0;
+	while (d = b%c)
+	{
+		b = c;
+		c = d;
+	}
+	return c;
+}
+int main()
+{
+	int n = 0;
+	int a = 0;
+	vector<int> num;
+	while (cin >> n >> a)
+	{
+		num.resize(n);
+		for (int i = 0; i<n; i++)
+		{
+			cin >> num[i];
+		}
+		for (int k = 0; k<n; k++)
+		{
+			if (num[k] <= a)
+			{
+				a += num[k];
+			}
+			else
+			{
+				a += gong_yue_num(num[k], a);
+			}
+		}
+		cout << a << endl;
+	}
+	return 0;
+}*/
+
 /*链接：https://www.nowcoder.com/questionTerminal/784efd40ed8e465a84821c8f3970b7b5?f=discussion
 来源：牛客网
 
