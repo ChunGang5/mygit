f5df3d7b705619a7e95b6f35f43e2fa43166c0f8
ChunGang5 <1330431376@qq.com>
2019-12-04 22:57:44 +0800

    报头处理完毕

---
+++ HTTP_Server/HttpServer.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "HttpServer.hpp"
+using namespace std;
+
+static void Usage(string proc)
+{
+	cout <<"Usage" << porc << "port"<<endl;
+	}
+int main(int argc,char *argv[])
+{
+	if(argc != 2)
+	{
+		Usage(argv[0]);
+		exit;
+		}
+	HttpServer *htp = new HttpServer(atoi(argv[1]));
+	htp->InitHttpServer();
+	htp->Start();
+
+	delete htp;
+	return 0;
+}
+
--- HTTP_Server/HttpServer.hpp
+++ HTTP_Server/HttpServer.hpp
@@ -1,18 +1,104 @@
+#pragma once
+
 #include <iostream>
-#include "ProlocolUtil"
+#include <pthread>
+#include "Protocol.hpp"
+#include "Util.hpp"
+
 using namespace std;
+
+class Sock{
+	private:
+		int sock;
+		int port;
+	public:
+		Sock(const int &port_):port(port_),sock(-1)	//Sock的构造函数
+		{}
+		void Socket()
+		{
+			sock = socket(AF_INET, SOCK_STREAM, 0);
+			if(sock < 0)
+			{
+				cerr << "sock cream error!" << endl;
+				exit(2);
+			}
+			int opt = 1;
+			setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));	//保证客户端想访问，服务器挂掉进入timewait
+		}
+		void Bind()
+		{
+			struct sockaddr_in local;
+			local.sin_family = AF_INET;
+			local.sin_addr.s_addr = htonl(INADDR_ANY);	//?参数
+			local.sin_port = htons(port);
+
+			if(bind(sock, (struct sockaddr*)&local, sizeof(local)) < 0)
+			{
+				cerr << "bind error!" << endl;
+				exit(3);
+			}
+
+		}
+		void Listen()
+		{
+			const int backlog = 5;	//链接队列的长度backog+1
+			if(listen(sock, backlog) < 0)
+			{
+				cerr << "listen error!" << endl;
+				exit(4);
+			}
+		}
+		int Accept()
+		{
+			struct sockaddr_in peer;
+			socklen_t len = sizeof(peer);
+			int fd = accept(sock, (struct sockaddr*)&peer, &len);
+			if(fd < 0)
+			{
+				ceer << "accept error!" <<endl;
+				exit(5);
+				return -1;
+			}
+			else
+			{
+				cout <<"get a new link......" << endl;
+				return fd;	
+			}
+		}
+		
+		~Sock()
+		{
+			if(Sock >= 0)
+			{
+				close(Sock);
+			}
+		}
+};
 
 class HttpServer{
 	private:
-		int listen_sock;
-		int port;
+		Sock sock;
 	public:
-		HttpServer();
-		void InitServer();
+		HttpServer(int port = DEFAULT_PORT):sock(port);
+		{}
+		void InitHttpServer()
+		{
+			sock.Socket();
+			sock.Bind();
+			sock.Accept();
+		}
+		void Start()
+		{
+			for(;;)
+			{
+				int sock_=sock.Accept();	//创建一个新fd
+				if(sock_>=0)
+				{
+					pthread_t tid;
+					int *p = new int(sock_);
+					pthread_creat(&tid, nullptr,Entry:: HandlerRequest, p);
 
-		~HttpServer();
-	
-
-	
-	
-};
+				}
+			}
+		}
+}
