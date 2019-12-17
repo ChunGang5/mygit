ea2b557f30ffd82b86e40babe8d56602927f5ec5
ChunGang5 <1330431376@qq.com>
2019-12-17 19:35:46 +0800

    找出第k小的数字

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -1,3 +1,26 @@
+/*查找一个数组的第K小的数，注意同样大小算一样大。 如  2 1 3 4 5 2 第三小数为3。
+https ://www.nowcoder.com/questionTerminal/204dfa6fcbc8478f993d23f693189ffd?f=discussion
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+	int n, k;
+	while (cin >> n){
+		int a[1000] = { 0 }, num;//使用位图进行去重
+		for (int i = 0; i<n; i++){
+			cin >> num;
+			a[num] = 1;
+		}
+		cin >> k;
+		vector<int> nums;
+		for (int i = 0; i<1000; i++){//按位得到有序的数组
+			if (a[i] == 1) nums.push_back(i);
+		}
+		cout << nums.at(k - 1) << endl;
+	}
+	return 0;
+}
+*/
 /*题目描述
 小易经常沉迷于网络游戏.有一次, 他在玩一个打怪升级的游戏, 他的角色的初始能力值为 a.在接下来的一段时间内, 他将会依次遇见n个怪物, 每个怪物的防御力为b1, b2, b3...bn.如果遇到的怪物防御力bi小于等于小易的当前能力值c, 那么他就能轻松打败怪物, 并 且使得自己的能力值增加bi; 如果bi大于c, 那他也能打败怪物, 但他的能力值只能增加bi 与c的最大公约数.那么问题来了, 在一系列的锻炼后, 小易的最终能力值为多少 ?
 输入描述 :
