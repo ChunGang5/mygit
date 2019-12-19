56b71d01d662e200c2204668d15be6341cd66399
ChunGang5 <1330431376@qq.com>
2019-12-19 23:22:00 +0800

    整数反转  溢出

---
+++ Leetcode/Leetcode/test.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string>
+using namespace std;
+int main()
+{
+	int num = 121;
+	string str;
+
+}
+////整数反转
+//int main()
+//{
+//	int a = 123;
+//	int i = 0;
+//	while (a)
+//	{
+//		i = i * 10 + a % 10;
+//		a = a / 10;
+//	}
+//	if (i<INT_MAX&&i>INT_MIN)
+//	{
+//		printf("%d\n", i);
+//	}
+//	
+//	system("pause");
+//	return 0;
+//}
