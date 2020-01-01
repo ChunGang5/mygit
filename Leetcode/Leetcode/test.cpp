c4418ba72892384b538cc28ffb3963bc73cbd630
ChunGang5 <1330431376@qq.com>
2020-01-01 22:28:19 +0800

    链表

--- Leetcode/Leetcode/test.cpp
+++ Leetcode/Leetcode/test.cpp
@@ -8,6 +8,41 @@ int main()
 	string str;
 
 }
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+	if (!head)
+	{
+		return NULL;
+	}
+	while (head != NULL && head->val)
+	{
+		head = head->next;
+	}
+	ListNode prev = head;
+	if (prev != NULL)
+	{
+		while (prev->next != NULL)
+		{
+			if (prev->next->val == val)
+			{
+				prev->next = prev->next->next;
+			}
+			else
+			{
+				prev = prev->next;
+			}
+		}
+	}
+	return head;
+}
 ////整数反转
 //int main()
 //{
