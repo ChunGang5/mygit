00bb8d13f17c9e2cb4939928bf80b43d53150a54
ChunGang5 <1330431376@qq.com>
2020-02-08 23:50:50 +0800

    练习

--- C++3/C++3/test.cpp
+++ C++3/C++3/test.cpp
@@ -89,17 +89,137 @@ using namespace std;
 //	return 0;
 //}
 
-int main()
-{
-	float hats, heads;
-	cout.setf(ios_base::fixed, ios_base::floatfield);	//迫使输出以定点表示法，防止程序将较大的值转换为E表示法
-	cin >> hats;
-	cin >> heads;
-	cin.get();
-	cout << hats + heads << endl;
-	cout << hats - heads << endl;
-	cout << hats * heads << endl;
-	cout << hats / heads << endl;
-	cin.get();
-	return 0;
-}
+//int main()
+//{
+//	float hats;
+//	int heads;
+//	cout.setf(ios_base::fixed, ios_base::floatfield);	//迫使输出以定点表示法，防止程序将较大的值转换为E表示法
+//	cin >> hats;
+//	cin >> heads;
+//	cin.get();
+//	cout << hats + heads << endl;
+//	cout << hats - heads << endl;
+//	cout << hats * heads << endl;
+//	cout << hats / heads << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//int main()
+//{
+//	int auks, bats, coots;
+//	auks = 19.99 + 11.99;	//整型提升
+//	bats = (int)19.99 + (int)11.99;
+//	coots = int(19.99) + int(11.99);
+//	cout << "auks = " << auks << endl;
+//	cout << "bats = " << bats << endl;
+//	cout << "coots = " << coots << endl;
+//	
+//	char ch = 'Z';
+//	cout << ch << endl;
+//	cout << int(ch) << endl;
+//	cout << (int)ch << endl;
+//	cout << static_cast<int>(ch) << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex3.1
+//int main()
+//{
+//	const int in_per = 12;
+//	cout << "Enter you height:___\b\b\b\a" ;
+//	int height = 0;
+//	cin >> height;
+//	cin.get();
+//	cout << height / in_per << endl;
+//	cout << height%in_per << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex3.2
+//int main()
+//{
+//	const int inch_per_inch = 12;
+//	const double inch_per_height = 0.0245;
+//	const double inch_per_ki = 2.2;
+//	
+//	cout << "Enter you height1___\b\b\b\a";
+//	double height1;
+//	cin >> height1;
+//	cin.get();
+//	cout << "Enter you height2___\b\b\b\a";
+//	double height2;
+//	cin >> height2;
+//	cin.get();
+//	cout << "Enter you weight___\b\b\b\a";
+//	double weight;
+//	cin >> weight;
+//	cin.get();
+//
+//	double a = height1 * inch_per_inch + height2;
+//	double b = weight / inch_per_ki;
+//	double c = a * inch_per_height;
+//	cout << b / (c*c) << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex3.3
+//int main()
+//{
+//	const int inch = 60;
+//	cout << "Enter a latitude in degress, minutes and seconds:" << endl;
+//	cout << "First,enter the degrees:__\b\b\a";
+//	double degress;
+//	cin >> degress;
+//	cout << "Next,enter the minutes of arc: __\b\b\a";
+//	double minutes;
+//	cin >> minutes;
+//	cout << "Finally,enter the seconds of arc: __\b\b\a";
+//	double seconds;
+//	cin >> seconds;
+//	cin.get();
+//	double a = degress + minutes / inch + seconds / inch / inch;
+//	cout << degress << " degress, " << minutes << " minutes, " << seconds << " seconds = " << a << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex3.4
+//int main()
+//{
+//	const int inch = 60;
+//	const int One_Day_hour = 24;
+//	cout << "Enter the number of seconds:";
+//	long seconds;
+//	cin >> seconds;
+//	cin.get();
+//	int day, minutes, hours, second_t;
+//	day = seconds / inch / inch / One_Day_hour;
+//	hours = seconds / inch / inch%One_Day_hour;
+//	minutes = seconds / inch%inch;
+//	second_t = seconds%inch;
+//	cout << seconds << " seconds = " << day << " days, " << hours << " hours, " << minutes << " minutes, " << second_t << " seconds.";
+//	cin.get();
+//	return 0;
+//}
+
+//Ex3.5
+//int main()
+//{
+//	cout << "Enter the world's population:";
+//	long long World_peo;
+//	cin >> World_peo;
+//	cin.get();
+//	cout << "Enter the population of the US:";
+//	long long US_peo;
+//	cin >> US_peo;
+//	cin.get();
+//	double a = 100*US_peo / World_peo;
+//	cout << "The population of the US is " << a<< " % of the world population." << endl;
+//	cin.get();
+//	return 0;
+//}
+
