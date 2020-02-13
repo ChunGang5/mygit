dea67d099c5c64c612a39e2b3285e9eb14f80a4e
ChunGang5 <1330431376@qq.com>
2020-02-13 21:45:48 +0800

    测试代码

---
+++ C++5/C++5/test.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<ctime>
+using namespace std;
+
+//int main()
+//{
+//	int by;
+//	cin >> by;
+//	cin.get();
+//	cout << "Counting by" << by << endl;
+//	for (int i = 0; i < 100; i = i + by)
+//		cout << i <<  endl;
+//	cin.get();
+//	return 0;
+//}
+
+//int main()
+//{
+//	cout << "Enter a word:";
+//	string word;
+//	cin >> word;
+//	cin.get();
+//	char temp;
+//	int i, j;
+//	for (j = 0, i = word.size() - 1; j < i; ++j, --i)
+//	{
+//		temp = word[i];
+//		word[i] = word[j];
+//		word[j] = temp;
+//	}
+//	cout << word << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//int main()
+//{
+//	char str1[5] = "def";
+//	char str2[5] = "bcg";
+//	if (strcmp(str1, str2)<0)
+//	{
+//		cout << "fgbhnjm" << endl;
+//	}
+//	else
+//	{
+//		cout << "no" << endl;
+//	}
+//	string str = "fghjk";
+//	str[0] =' g';
+//	cout << str << endl;
+//	cin.get();
+//	return 0;
+//}
+//
+//int main()
+//{
+//	float sea;
+//	cin >> sea;
+//	clock_t daliy = sea*CLOCKS_PER_SEC;
+//	clock_t start = clock();
+//	while (clock() - start < daliy)
+//		;
+//	cin.get();
+//	cout << "done\a\n";
+//	cin.get();
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	char ch;
+//	int count = 0;
+//	cin.get(ch).get();
+//	
+//	while (ch != '#')
+//	{
+//		cout << ch; 
+//		++count;
+//		cin.get(ch);
+//	}
+//	cin.get();
+//	cout << endl << count << " cna\n";
+//	cin.get();
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*char ch;
+//	int count = 0;
+//	cin.get(ch);
+//	while (cin.fail() == false)
+//	{
+//		cout << ch;
+//		++count;
+//		cin.get(ch);
+//	}
+//	cin.get();
+//	cout << endl << count << " characters read\a\n";*/
+//	cout.put(97);
+//	cin.get();
+//	return 0;
+//}
+
