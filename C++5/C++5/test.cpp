ca6a9427d8683a5d9f05948074e4c38558a7105b
ChunGang5 <1330431376@qq.com>
2020-02-14 21:54:34 +0800

    复习题

--- C++5/C++5/test.cpp
+++ C++5/C++5/test.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<string>
+#include<array>
+#include<vector>
 #include<cstring>
 #include<ctime>
 using namespace std;
@@ -101,6 +103,182 @@ using namespace std;
 //	cin.get();
 //	cout << endl << count << " characters read\a\n";*/
 //	cout.put(97);
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.1
+//int main()
+//{
+//	int a, b;
+//	cout << "Enter tow number:";
+//	cin >> a >> b;
+//	cin.get();
+//	int sum = 0;
+//	for (a; a <= b; a++)
+//	{
+//		sum += a;
+//	}
+//	cout << sum << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.2
+//int main()
+//{
+//	cout.setf(ios_base::fixed, ios_base::floatfield);
+//	array<long double, 101> arr1;
+//	arr1[0] = arr1[1] = 1;
+//	for (int i = 2; i < 101; i++)
+//	{
+//		arr1[i] = i*arr1[i - 1];
+//	}
+//	for (int i = 0; i < 101; i++)
+//	{
+//		cout << arr1[i] << endl;
+//	}
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.3
+//int main()
+//{
+//	int num = 0,sum=0;
+//	while ((cin >> num)&&num!=0)
+//	{
+//		sum += num;
+//		cout << sum << endl;
+//	}
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.4
+//int main()
+//{
+//	int menoy = 100;
+//	double Cleo=0, Daphone=0;
+//	int i = 0;
+//	for (i = 0; Cleo <= Daphone; i++)
+//	{
+//		Daphone = Daphone + menoy * 0.1 ;
+//		Cleo = (100 + Cleo)*0.05 + Cleo;
+//	}
+//	cout << i << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.5
+//const int MONTHS = 12;
+//string months[MONTHS] 
+//= { "January", "February", "March", "April", "May", "June", "July", "August", "September","October","November","December"};
+
+//int main()
+//{
+//	int sum = 0;
+//	double sales[MONTHS] = {};
+//	for (int i = 0; i < MONTHS; i++)
+//	{
+//		cin >> sales[i];
+//		sum += sales[i];
+//	}
+//	cin.get();
+//	for (int i = 0; i < MONTHS; i++)
+//	{
+//		cout << months[i] << " " << sales[i ]<<endl;
+//	}
+//	cout << sum << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.6
+//int main()
+//{
+//	double sales[12][3] = {};
+//	int sum = 0;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for (int j = 0; j < MONTHS; j++)
+//		{
+//			cin >> sales[i][j];
+//			sum += sales[i][j];
+//		}
+//	}
+//	cin.get();
+//	cout << sum << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.7
+//struct Car
+//{
+//	string name;
+//	int year;
+//};
+//
+//int main()
+//{
+//	
+//	int num;
+//	cout << "How many cars do you have?" << endl;
+//	cin >> num;
+//	cin.get();
+//	Car *pt = new Car[num];
+//	for (int i = 0; i < num; i++)
+//	{
+//		getline(cin, pt[i].name);
+//		cin >> pt[i].year;
+//		cin.get();
+//	}
+//	for (int i = 0; i < num; i++)
+//	{
+//		cout << pt[i].year << " " << pt[i].name << endl;
+//	}
+//	cin.get();
+//	delete[] pt; 
+//	return 0;
+//}
+
+//Ex5.8
+//int main()
+//{
+//	char word[20];
+//	int sum = 0;
+//	cin >> word;
+//	while (strcmp(word, "done"))
+//	{
+//		sum += 1;
+//		cin >> word;
+//	}
+//	//cin.get();
+//	cout << sum << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//Ex5.9
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//	cin.get();
+//	for (int i = 0; i < num; i++)
+//	{
+//		for (int j = 0; j < num - i-1; j++)
+//		{
+//			cout << ".";
+//		}
+//		for (int k = 0; k <= i; k++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
 //	cin.get();
 //	return 0;
 //}
