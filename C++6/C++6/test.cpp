8dd4f7e14f059ad32ebbf0b6ed3d688c818cc2e6
ChunGang5 <1330431376@qq.com>
2020-02-14 23:15:31 +0800

    测试代码

---
+++ C++6/C++6/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char ch;
+	cout << "Type, and i shall repet.\n";
+	cin.get(ch);
+	while (ch != '.')
+	{
+		if (ch == '\n')
+		{
+			cout << ch;
+		}
+		else
+			cout << ch+1;//?
+		cin.get(ch);
+	}
+	cout << "\nPlease excuse the slight confusion.\n";
+	cin.get();
+	return 0;
+}
