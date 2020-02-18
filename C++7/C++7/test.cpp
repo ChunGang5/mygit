5ed2b6f9e05853756e2f82467d4f7025e13fc24d
ChunGang5 <1330431376@qq.com>
2020-02-18 23:26:48 +0800

    示例

---
+++ C++7/C++7/test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<array>
+
+using namespace std;
+
+//const int ArSize = 8;
+//int sum_arr(int *arr, int n);	//函数头或函数原型中是一样的与int arr[];都代表着是一个指针，也就是数组首元素地址
+//
+//int main()
+//{
+//	int cookies[ArSize] = { 1, 2, 5, 8, 6, 4, 7, 9 };
+//	int sum = sum_arr(cookies, ArSize);
+//	cout << sum << endl;
+//	cin.get();
+//	return 0;
+//}
+//int sum_arr(int arr[], int n)
+//{
+//	int total = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		total += arr[i];
+//	}
+//	return total;
+//}
+
