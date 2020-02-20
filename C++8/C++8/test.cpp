ecff584b3b0214fea3ed112c440cc41d294ac59f
ChunGang5 <1330431376@qq.com>
2020-02-20 22:40:31 +0800

    用例

---
+++ C++8/C++8/test.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<array>
+
+using namespace std;
+
+//int main()
+//{
+//	int rats = 101;
+//	int *pt = &rats;
+//	int &rodents = *pt;
+//	int bunnies = 50;
+//	pt = &bunnies;
+//	return 0;
+//}
+
+//double refcube(const double &n)
+//{
+//	return n*n*n;
+//}
+//void swapr(const double & a, const double & b)
+//{
+//	int temp;
+//	temp = a;
+//	a = b;
+//	b = temp;
+//}
+//int main()
+//{
+//	double x = 3.0;
+//	double y = 5.0;
+//	int c = 2;
+//	int b = 5;
+//	cout << refcube(x) << endl;
+//	swapr(c, b);
+//	cin.get();
+//	return 0;
+//}
+
