2695fdaf7a37f515c6881cf6be90c44794a2c5ad
ChunGang5 <1330431376@qq.com>
2020-03-04 22:51:22 +0800

    字串

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -480,3 +480,40 @@ int main()
 //	}
 //	return 0;
 //}
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+	string s;
+	string t;
+	while (cin>>s)
+	{
+	cin >> t;
+	int count = 0;
+	int start = 0;
+	int j = 0;
+	for (int i = 0; i < s.size(); ++i)
+	{
+		start = i;
+		for (j = 0; j < t.size(); ++j)
+		{
+			if (s[i] == t[j])
+				++i;
+			else
+				break;
+		}
+		if (j == t.size())
+		{
+			++count;
+			i = i - 1;
+		}
+		else
+			i = start;
+	}
+	cout << count << endl;
+	}
+return 0;
+}
