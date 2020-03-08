c2b98597159bc49fca8f14125c6a8937d19c64f9
ChunGang5 <1330431376@qq.com>
2020-03-08 18:07:30 +0800

    字符串处理，对比

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -645,3 +645,68 @@ int main()
 //	}
 //	return 0;
 //}
+
+
+//#include <iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	string t;
+//	while (cin >> s)
+//	{
+//		cin >> t;
+//		int count = 0;
+//		int start = 0;
+//		int j = 0;
+//		for (int i = 0; i < s.size(); ++i)
+//		{
+//			start = i;
+//			for (j = 0; j < t.size(); ++j)
+//			{
+//				if (s[i] == t[j])
+//					++i;
+//				else
+//					break;
+//			}
+//			if (j == t.size())
+//			{
+//				++count;
+//				i = i - 1;
+//			}
+//			else
+//				i = start;
+//		}
+//		cout << count << endl;
+//	}
+//	return 0;
+//}
+//
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		cin.get();
+//		while (n--)
+//		{
+//			string names;
+//			getline(cin, names);
+//			if (names.find(',') != -1 || names.find(' ') != -1)
+//			{
+//				names.insert(names.begin(), '"');
+//				names.insert(names.end(), '"');
+//			}
+//			if (n == 0)
+//				cout << names << endl;
+//			else
+//				cout << names << ", ";
+//		}
+//	}
+//	cin.get();
+//	return 0;
+//}
