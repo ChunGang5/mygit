402a9e30b19a961da9a712f4309417f61b6214a1
ChunGang5 <1330431376@qq.com>
2020-03-09 22:23:37 +0800

    深度优先搜索

--- C++9/C++9/test.cpp
+++ C++9/C++9/test.cpp
@@ -54,14 +54,222 @@ using namespace std;
 //	return 0;
 //}
 
-int main()
-{
-	int a = 10;
-	int& ra = a;
-	ra = 20;
-	int* pa = &a;
-	*pa = 20;
-	cout << sizeof(nullptr) << endl;
-	cin.get();
-	return 0;
-}
+//int main()
+//{
+//	int a = 10;
+//	int& ra = a;
+//	ra = 20;
+//	int* pa = &a;
+//	*pa = 20;
+//	cout << sizeof(nullptr) << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*struct Test
+//	{
+//		char a;
+//		int c;
+//		short d;
+//	};*/
+//	class A1 {
+//	public:
+//		void f1(){}
+//	private:
+//		int _a;
+//	};
+//	// 类中仅有成员函数
+//	class A2 {
+//	public:
+//		void f2() {}
+//	};
+//	// 类中什么都没有---空类
+//	class A3
+//	{};
+//	cout << sizeof(A1) << endl;
+//	cout << sizeof(A2) << endl;
+//	cout << sizeof(A3) << endl;
+//	cin.get();
+//	return 0;
+//}
+
+//class A
+//{
+//public:
+//	void PrintA()
+//	{
+//		cout << _a << endl;
+//	}
+//	void Show()
+//	{
+//		cout << "Show()" << endl;
+//	}
+//private:
+//	int _a;
+//};
+//int main()
+//{
+//	A* p = NULL;
+//	p->PrintA();
+//	p->Show();
+//	cin.get();
+//	return 0;
+//}
+
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//		_year = 1900;
+//		_month = 1;
+//		_day = 1;
+//	}
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//// 以下测试函数能通过编译吗？
+//void Test()
+//{
+//	Date d1;
+//}
+
+//class String
+//{
+//public:
+//	String(const char* str = "jack")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//	}
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//int main()
+//{
+//	String s1("hello");
+//	String s2(s1);
+//	cin.get();
+//	return 0;
+//}
+
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	// bool operator==(Date* this, const Date& d2)
+//	// 这里需要注意的是，左操作数是this指向的调用函数的对象
+//	bool operator==(const Date& d2)
+//	{
+//		return _year == d2._year
+//			&& _month == d2._month
+//			&& _day == d2._day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//void Test()
+//{
+//	Date d1(2018, 9, 26);
+//	Date d2(2018, 9, 27);
+//	cout << (d1 == d2) << endl;
+//}
+//int main()
+//{
+//	void Test();
+//	cin.get();
+//	return 0;
+//}
+
+//class String
+//{
+//public:
+//	String(const char* str = "jack")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//	}
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//int main()
+//{
+//	String s1("hello");
+//	String s2("world");
+//	s1 = s2;
+//	cin.get();
+//	return 0;
+//}
+
+// write your code here cpp
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+////通过vector创建的二维数组进行递归调用，把已经走过的结点标记成'1',避免访问过的
+////结点重复计数
+//void BlackCount(vector<vector<char>>& v, int x, int y, int m, int n, int& count)
+//{
+//	//count通过引用的方式，正好递归调用进行计数
+//	if (x<0 || y<0 || x >= m || y >= n || v[x][y] == '1' || v[x][y] == '#')
+//		return;
+//	count++;
+//	v[x][y] = '1';
+//	BlackCount(v, x - 1, y, m, n, count);
+//	BlackCount(v, x, y - 1, m, n, count);
+//	BlackCount(v, x + 1, y, m, n, count);
+//	BlackCount(v, x, y + 1, m, n, count);
+//}
+//
+//int main()
+//{
+//	int m, n;
+//	while (cin >> m >> n)
+//	{
+//		int x, y;
+//		int count = 0;
+//		vector<vector<char>> v(m, vector<char>(n, 0));
+//		for (size_t i = 0; i<m; i++)
+//		{
+//			for (size_t j = 0; j<n; j++)
+//			{
+//				cin >> v[i][j];
+//				if (v[i][j] == '@')
+//				{
+//					x = i;//x标记起始i结点
+//					y = j;//y表示起始j结点
+//				}
+//			}
+//		}
+//		BlackCount(v, x, y, m, n, count);
+//		cout << count << endl;
+//	}
+//	return 0;
+//}
