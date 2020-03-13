1b941f7c9a2b3fdcc048cde7a211f295a1f674f5
ChunGang5 <1330431376@qq.com>
2020-03-13 23:14:44 +0800

    。

--- C++9/C++9/test.cpp
+++ C++9/C++9/test.cpp
@@ -409,60 +409,68 @@ using namespace std;
 //	int _day;
 //};
 
-#include<iostream>
-using namespace std;
-int iConverDateToDay(int year, int month, int day)
-{
-	if (year <= 0 || month <= 0 || day <= 0 || month>12 || day>31)
-	{
-		return -1;
-	}
-	int lemp[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	int Nolemp[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	bool flag;
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-	{
-		flag = true;
-	}
-	else
-	{
-		flag = false;
-	}
-	if (flag)
-	{
-		if (day>lemp[month - 1])
-		{
-			return -1;
-		}
-	}
-	else
-	{
-		if (day>Nolemp[month - 1])
-		{
-			return -1;
-		}
-	}
-	int temp = 0;
-	for (int i = 0; i<month - 1; i++)
-	{
-		if (flag)
-		{
-			temp += lemp[i];
-		}
-		else
-			temp += Nolemp[i];
-	}
-	return temp + day;
-}
+//#include<iostream>
+//using namespace std;
+//int iConverDateToDay(int year, int month, int day)
+//{
+//	if (year <= 0 || month <= 0 || day <= 0 || month>12 || day>31)
+//	{
+//		return -1;
+//	}
+//	int lemp[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//	int Nolemp[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//	bool flag;
+//	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//	{
+//		flag = true;
+//	}
+//	else
+//	{
+//		flag = false;
+//	}
+//	if (flag)
+//	{
+//		if (day>lemp[month - 1])
+//		{
+//			return -1;
+//		}
+//	}
+//	else
+//	{
+//		if (day>Nolemp[month - 1])
+//		{
+//			return -1;
+//		}
+//	}
+//	int temp = 0;
+//	for (int i = 0; i<month - 1; i++)
+//	{
+//		if (flag)
+//		{
+//			temp += lemp[i];
+//		}
+//		else
+//			temp += Nolemp[i];
+//	}
+//	return temp + day;
+//}
+//int main()
+//{
+//	int year, month, day;
+//	while (cin >> year >> month >> day)
+//	{
+//		cin.get();
+//		cout << iConverDateToDay(year, month, day) << endl;
+//		cin.get();
+//		return 0;
+//	}
+//	return 0;
+//}
+
+const int MAX = 50;
 int main()
 {
-	int year, month, day;
-	while (cin >> year >> month >> day)
-	{
-		cin.get();
-		cout << iConverDateToDay(year, month, day) << endl;
-		cin.get();
-		return 0;
-	}
+	cout << MAX << endl;
+	cin.get();
 	return 0;
 }
