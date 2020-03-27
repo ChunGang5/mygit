9b73c5cc1b0a966e462abfebf6cdc64a3b5b4690
ChunGang5 <1330431376@qq.com>
2020-03-27 23:10:19 +0800

    数据库管理模块增加

--- DocFastSearchTool/DocFastSearchTool/DocFastSearchTool.cpp
+++ DocFastSearchTool/DocFastSearchTool/DocFastSearchTool.cpp
@@ -6,6 +6,10 @@ void Test_Directory()
 	string path = "G:\\桌面\\比特科技";
 	vector<string> subfile, subdir;
 	DirectoryList(path,subfile,subdir);
+	for (auto &e : subfile)
+		cout << e << endl;
+	for (auto &e : subdir)
+		cout << e << endl;
 }
 int main(int argc, char *argv[])
 {
