165ebf54db521be502cc5c067c879e5ccfe31cef
ChunGang5 <1330431376@qq.com>
2020-04-07 22:12:16 +0800

    环形链表

--- 链表复习/链表复习/test.cpp
+++ 链表复习/链表复习/test.cpp
@@ -176,6 +176,45 @@ val(x), next(NULL) {
 //	}
 //};
 
+//方法二  双指针法
+/*
+struct ListNode {
+int val;
+struct ListNode *next;
+ListNode(int x) :
+val(x), next(NULL) {
+}
+};*/
+//class Solution {
+//public:
+//	ListNode* FindKthToTail(ListNode* pListHead, unsigned int k)
+//	{
+//		if (pListHead == NULL || k == 0)    //判断链表是否为空，还有k的不正确，如果k=0，倒数第零个就没有啊
+//		{
+//			return NULL;
+//		}
+//		ListNode *cur = pListHead;
+//		ListNode *last = pListHead;
+//		while (k--)    //k<0这种情况循环根本不进去，这是这个问题的一个bug处
+//		{
+//			if (last == NULL)
+//			{
+//				return NULL;
+//			}
+//			else
+//			{
+//				last = last->next;
+//			}
+//		}
+//		while (last)
+//		{
+//			cur = cur->next;
+//			last = last->next;
+//		}
+//		return cur;
+//	}
+//};
+
 
 //判断链表是否带环
 /**
@@ -204,3 +243,198 @@ val(x), next(NULL) {
 //		return false;   //不能放在循环体内判断不相等的情况，不然大部分情况下一定是false
 //	}
 //};
+
+
+//合并两个有序单链表
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+//class Solution {
+//public:
+//	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+//	{
+//		ListNode* cur1 = l1;
+//		ListNode* cur2 = l2;
+//		ListNode* newhead = NULL;
+//		ListNode* tail = NULL;    //标识新链表最后一个节点
+//		if (l1 == NULL)    //先给一个头节点
+//			return l2;
+//		if (l2 == NULL)
+//			return l1;
+//		if (cur1->val <= cur2->val)
+//		{
+//			newhead = cur1;
+//			cur1 = cur1->next;
+//		}
+//		else{
+//			newhead = cur2;
+//			cur2 = cur2->next;
+//		}
+//		tail = newhead;
+//		while (cur1&&cur2)
+//		{
+//			if (cur1->val <= cur2->val)
+//			{
+//				tail->next = cur1;
+//				cur1 = cur1->next;
+//			}
+//			else
+//			{
+//				tail->next = cur2;
+//				cur2 = cur2->next;
+//			}
+//			tail = tail->next;
+//		}
+//		//遇到有一个链表遍历完的情况
+//		if (cur1 == NULL)
+//			tail->next = cur2;
+//		if (cur2 == NULL)
+//			tail->next = cur1;
+//		return newhead;
+//	}
+//};
+
+ 
+//链表分割，--注意审题，x不是链表原有的节点值，只是给了一个值而已
+/*
+struct ListNode {
+int val;
+struct ListNode *next;
+ListNode(int x) : val(x), next(NULL) {}
+};*/
+//class Partition {
+//public:
+//	ListNode* partition(ListNode* pHead, int x) {
+//		ListNode BigHead(0);    //这个题有点怪病，非要用结构体初始化才不出错
+//		ListNode LittleHead(0);
+//		ListNode* BigTird = &BigHead;
+//		ListNode* LittleTird = &LittleHead;
+//		ListNode* cur = pHead;
+//		if (pHead == NULL)
+//		{
+//			return NULL;
+//		}
+//		while (cur)
+//		{
+//			if (cur->val<x)
+//			{
+//				LittleTird->next = cur;
+//				LittleTird = cur;
+//			}
+//			else
+//			{
+//				BigTird->next = cur;
+//				BigTird = cur;
+//			}
+//			cur = cur->next;
+//		}
+//		LittleTird->next = BigHead.next;
+//		BigTird->next = NULL;    //链表最末尾要指向NULL，不然就不完整，就会乱访问，出错
+//		return LittleHead.next;    //因为两个链表的头节点放的数都是0
+//	}
+//};
+
+
+//回文链表
+/*
+struct ListNode {
+int val;
+struct ListNode *next;
+ListNode(int x) : val(x), next(NULL) {}
+};*/
+//class PalindromeList {
+//public:
+//	bool chkPalindrome(ListNode* A) {
+//		int array[900] = { 0 };
+//		int size = 0;
+//		ListNode* cur = A;
+//		while (cur)
+//		{
+//			array[size++] = cur->val;
+//			cur = cur->next;
+//		}
+//		int left = 0;
+//		int right = size - 1;
+//		while (left < right)
+//		{
+//			if (array[left] != array[right])
+//			{
+//				return false;
+//			}
+//			left++;
+//			right--;
+//		}
+//		return true;
+//	}
+//};
+
+
+//相交链表的第一个节点
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+//class Solution {
+//public:
+//	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+//	{
+//		if (NULL == headA || NULL == headB)    //两个链表有一个为空，或都为空
+//		{
+//			return NULL;
+//		}
+//		ListNode* curA = headA;
+//		ListNode* curB = headB;
+//		int sizeA = 0;
+//		int sizeB = 0;
+//		while (curA)
+//		{
+//			sizeA++;
+//			curA = curA->next;
+//		}
+//		while (curB)
+//		{
+//			sizeB++;
+//			curB = curB->next;
+//		}
+//
+//		if (curB != curA)      //两个链表不为空，但不相交
+//			return NULL;
+//
+//		int n = sizeA - sizeB;
+//		//链表A比B长n个节点，的先遍历n次
+//		curA = headA;
+//		curB = headB;
+//		if (n>0)
+//		{
+//			while (n--)
+//			{
+//				curA = curA->next;
+//			}
+//
+//		}
+//		else    //B比A长
+//		{
+//			while (n++)
+//			{
+//				curB = curB->next;
+//			}
+//		}
+//		while (curA != curB)
+//		{
+//			curA = curA->next;
+//			curB = curB->next;
+//		}
+//		return curA;
+//	}
+//};
+
+
