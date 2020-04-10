030b5433566a8c47c8728cbe5a85436bac539b77
ChunGang5 <1330431376@qq.com>
2020-04-10 21:57:19 +0800

    插入

--- 双向链表/双向链表/DList.cpp
+++ 双向链表/双向链表/DList.cpp
@@ -1,56 +1,58 @@
-//#include "DList.h"
-//#include<assert.h>
-//
-//DListNode* BuyDListNode(int val)
-//{
-//	DListNode* newNode = (DListNode*)new(DListNode);
-//	if (NULL == newNode)
-//	{
-//		assert(0);
-//		return NULL;
-//	}
-//	newNode->next = NULL;
-//	newNode->prev = NULL;
-//	newNode->val = val;
-//
-//	return newNode;
-//}
-//
-//DListNode* CreatDList()
-//{
-//	DListNode* head = BuyDListNode(0);
-//	head->next = head;
-//	head->prev = head;
-//	return head;
-//}
-//void DListNodePushBack(DListNode* pHead, int val)
-//{
-//	if (NULL == pHead)
-//	{
-//		return;
-//	}
-//	DListNode* newNode = BuyDListNode(val);
-//	newNode->prev = pHead->prev;
-//	newNode->next = pHead;
-//	newNode->prev->next = newNode;
-//	pHead->prev = newNode;
-//}
-//
-//int main()
-//{
-//	DListNode* p1=BuyDListNode(0);
-//	DListNode* p2=BuyDListNode(1);
-//	DListNode* p3=BuyDListNode(2);
-//
-//	p1->next = p2;
-//	p2->next = p3;
-//	p2->prev = p1;
-//	p3->prev = p2;
-//	DListNodePushBack(p1, 3);
-//	cout << p3->next->val << endl;
-//	cin.get();
-//	return 0;
-//}
+#include "DList.h"
+#include<assert.h>
+
+DListNode* BuyDListNode(int val)
+{
+	DListNode* newNode = (DListNode*)new(DListNode);
+	if (NULL == newNode)
+	{
+		assert(0);
+		return NULL;
+	}
+	newNode->next = NULL;
+	newNode->prev = NULL;
+	newNode->val = val;
+
+	return newNode;
+}
+
+DListNode* CreatDList()
+{
+	DListNode* head = BuyDListNode(0);
+	head->next = head;
+	head->prev = head;
+	return head;
+}
+void DListNodePushBack(DListNode* pHead, int val)
+{
+	if (NULL == pHead)
+	{
+		return;
+	}
+	DListNode* newNode = BuyDListNode(val);
+	newNode->prev = pHead->prev;
+	newNode->next = pHead;
+	newNode->prev->next = newNode;
+	pHead->prev = newNode;
+}
+
+int main()
+{
+	DListNode* p1=BuyDListNode(0);
+	DListNode* p2=BuyDListNode(1);
+	DListNode* p3=BuyDListNode(2);
+
+	p1->next = p2;
+	p2->next = p3;
+	p2->prev = p1;
+	p3->prev = p2;
+	DListNodePushBack(p1, 3);
+	cout << p3->next->val << endl;
+	cin.get();
+	return 0;
+}
+
+
 
 
 
