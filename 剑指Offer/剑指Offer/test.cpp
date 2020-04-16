23e51176d590216a2ae40fbde8d55e9ca4f89a2e
ChunGang5 <1330431376@qq.com>
2020-04-16 23:38:05 +0800

    青蛙跳台阶

---
+++ 剑指Offer/剑指Offer/test.cpp
@@ -0,0 +1,122 @@
+//10：斐波那契数列_非递归求法，O(n)
+//class Solution {
+//public:
+//	int Fibonacci(int n)
+//	{
+//		if (n == 0)
+//		{
+//			return 0;
+//		}
+//		if (n == 1)
+//		{
+//			return 1;
+//		}
+//		long long fibone = 0;
+//		long long fibtwo = 1;
+//		long long fibn = 0;
+//		for (int i = 2; i <= n; i++)
+//		{
+//			fibn = fibtwo + fibone;
+//			fibone = fibtwo;
+//			fibtwo = fibn;
+//		}
+//		return fibn;
+//	}
+//};
+
+//拓展：青蛙跳台阶问题
+//当青蛙第一次跳一节的时候，还要f(n-1)种跳法，第一次条跳2阶时，剩下有f(n-2)种跳法
+//class Solution {
+//public:
+//	int jumpFloor(int number) {
+//		if (number == 1)
+//		{
+//			return 1;
+//		}
+//		if (number == 2)
+//		{
+//			return 2;
+//		}
+//		else
+//		{
+//			return jumpFloor(number - 1) + jumpFloor(number - 2);
+//		}
+//	}
+//};
+
+//非递归
+//class Solution {
+//public:
+//	int jumpFloor(int number)
+//	{
+//		if (number == 1)
+//		{
+//			return 1;
+//		}
+//		if (number == 2)
+//		{
+//			return 2;
+//		}
+//		long long jumpone = 1;
+//		long long jumptwo = 2;
+//		long long jumpN = 0;
+//		for (int i = 2; i<number; i++)	//注意这个题对应的斐波那契数列是从1开始的，所以i不能=n
+//		{
+//			jumpN = jumpone + jumptwo;
+//			jumpone = jumptwo;
+//			jumptwo = jumpN;
+//		}
+//		return jumpN;
+//	}
+//};
+
+//04:二维数组中的查找
+//class Solution {
+//public:
+//	//此题只能从左下角或者右上角求解，不然的话就会产生判断的不确定
+//	bool Find(int target, vector<vector<int> > array) {
+//		int rowCount = array.size();
+//		int colCount = array[0].size();
+//		int row = 0;
+//		int col = colCount - 1;
+//		bool found = false;    //标志很重要
+//		if (rowCount>0 && colCount>0)    //判断数组是否有效
+//		{
+//			while (row<rowCount && col >= 0)    //判断数组是否越界
+//			{
+//				if (target == array[row][col])
+//				{
+//					found = true;
+//					break;
+//				}
+//				else if (target < array[row][col])
+//				{
+//					--col;
+//				}
+//				else
+//					++row;
+//			}
+//		}
+//		return found;
+//	}
+//};
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a;
+	char *ptr1 = "-12345";
+
+	a = atoi(ptr1);
+
+	printf("a = %d,%d/n", a);
+	cin.get();
+
+	return 0;
+}
