a8aca1df6d37e558bd95548dec37e7db7b2f6c36
ChunGang5 <1330431376@qq.com>
2020-05-12 23:02:58 +0800

    选择

--- 排序/排序/test.cpp
+++ 排序/排序/test.cpp
@@ -18,32 +18,55 @@ using namespace std;
 //}
 
 //希尔排序
-void ShellSort(int arr[], int n,int gap)	//差
-{
-	while (gap >= 1)
-	{
-		int key;
-		for (int i = gap; i < n; i++)	//五个数只需要排四次
-		{
-			key = arr[i];
-			int end = i - gap;
-			while (arr[end] > key && end >= 0)
-			{
-				arr[end + gap] = arr[end];
-				end-=gap;
-			}
-			arr[end + gap] = key;
-		}
-		gap--;
-	}
-	
-}
+//void ShellSort(int arr[], int n,int gap)	//差
+//{
+//	while (gap >= 1)
+//	{
+//		int key;
+//		for (int i = gap; i < n; i++)	//五个数只需要排四次
+//		{
+//			key = arr[i];
+//			int end = i - gap;
+//			while (arr[end] > key && end >= 0)
+//			{
+//				arr[end + gap] = arr[end];
+//				end-=gap;
+//			}
+//			arr[end + gap] = key;
+//		}
+//		gap--;
+//	}
+//	
+//}
+
+//直接选择排序
+//void QuickSelectionSort(int arr[], int n)
+//{
+//	for (int i = 0; i < n - 1; i++)
+//	{
+//		int min;
+//		int temp;
+//		for (int j = i + 1; j < n; j++)
+//		{
+//			min = i;
+//			if (arr[j] < arr[min])
+//			{
+//				min = j;
+//			}
+//		}
+//		//交换放在外层，内部for只是找出最值的下标
+//		temp = arr[i];
+//		arr[i] = arr[min];
+//		arr[min] = temp;
+//	}
+//}
 int main()
 {
-	int arr[20] = { 2, 4, 3, 6, 5,51,7,8,215,95, 151,488,51,31,66,848,36,41,626,6326};
+	int arr[5] = { 2, 4, 3, 6, 5};
 	int n = sizeof(arr) / sizeof(arr[0]);
 	//InserSort(arr, n);
-	ShellSort(arr, n, 3);
+	//ShellSort(arr, n, 3);
+	//QuickSelectionSort(arr, n);
 	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
 	{
 		cout << arr[i] << endl;
