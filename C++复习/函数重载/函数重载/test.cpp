e03c24b20e192ab3897928379921d0f7dcbe7e02
ChunGang5 <1330431376@qq.com>
2020-05-29 22:29:40 +0800

    动态内存管理

--- C++复习/函数重载/函数重载/test.cpp
+++ C++复习/函数重载/函数重载/test.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
+#include<vector>
+
 #include"Head.h"
 using namespace std;
 //double Add(double a, double b)
@@ -43,32 +45,83 @@ using namespace std;
 //	return 0;
 //}
 
+//int main()
+//{
+//	/*char* s = "computer";
+//	printf("%s\n", s);
+//	printf("%10s\n", s);
+//	printf("%.5s\n", s);
+//	printf("%0.10s\n", s);
+//	printf("%-.5s\n", s);*/
+//	//printf("%*.*s\n", s);
+//	//printf("Hello\n");
+//	/*int arr[5] = { 1, 2, 3, 4, 5 };
+//	int* pr= (int*)arr[4];*/
+//	/*int a[] = { 1, 2, 3, 4 };
+//	int *b = a;
+//	*b += 2;
+//	*(b + 2) = 2;
+//	b++;
+//	printf("%d, %d\n", *b, *(b + 2));
+//	cin.get();
+//	return 0;*/
+//	vector<int> m(10);
+//	vector<vector<int>> grid(10, vector<int>(10, 1));
+//	int ret = Add(1, 7);
+//	cout << ret << endl;
+//	ret = Add(7, 9);
+//	cout << ret << endl;
+//	cin.get();
+//	return 0;
+//}
+
+
+//class A
+//{
+//public:
+//	static const int a = 1;
+//	static const double c= 13.50;
+//	static int b;
+//};
+//int A::b = 1;
+class Test
+{
+public:
+	Test()
+		: _data(0)
+	{
+		cout << "Test():" << this << endl;
+	}
+	~Test()
+	{
+		cout << "~Test():" << this << endl;
+	}
+private:
+	int _data;
+};
+void Test2()
+{
+	// 申请单个Test类型的空间
+	Test* p1 = (Test*)malloc(sizeof(Test));
+	free(p1);
+	// 申请10个Test类型的空间
+	Test* p2 = (Test*)malloc(sizeof(Test) * 10);
+	free(p2);
+}
+void Test2()
+{
+	// 申请单个Test类型的对象
+	Test* p1 = new Test;
+	delete p1;
+	// 申请10个Test类型的对象
+	Test* p2 = new Test[10];
+	delete[] p2;
+}
 int main()
 {
-	/*char* s = "computer";
-	printf("%s\n", s);
-	printf("%10s\n", s);
-	printf("%.5s\n", s);
-	printf("%0.10s\n", s);
-	printf("%-.5s\n", s);*/
-	//printf("%*.*s\n", s);
-	//printf("Hello\n");
-	/*int arr[5] = { 1, 2, 3, 4, 5 };
-	int* pr= (int*)arr[4];*/
-	/*int a[] = { 1, 2, 3, 4 };
-	int *b = a;
-	*b += 2;
-	*(b + 2) = 2;
-	b++;
-	printf("%d, %d\n", *b, *(b + 2));
-	cin.get();
-	return 0;*/
-
-	int ret = Add(1, 7);
-	cout << ret << endl;
-	ret = Add(7, 9);
-	cout << ret << endl;
-	cin.get();
+	/*int *p = new int(100);
+	int *ptr = new int[10]{1,2,3,4,5,7,6};
+	int *ptr1 = (int*)malloc(sizeof(int));*/
+	Test2();
 	return 0;
 }
-
